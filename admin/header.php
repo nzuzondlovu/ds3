a7d5c91063b159d97dae0cf5c982509e1493a201
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-04-28 21:11:57 +0200

    Added new links

--- admin/header.php
+++ admin/header.php
@@ -280,10 +280,16 @@
                             <a href="bookings.php"><i class="fa fa-table fa-fw"></i> View Bookings</a>
                         </li>
                         <li>
+                            <a href="cats.php"><i class="fa fa-edit fa-fw"></i> Categories</a>
+                        </li>
+                        <li>
                             <a href="additem.php"><i class="fa fa-edit fa-fw"></i> Add Product</a>
                         </li>
                         <li>
                             <a href="items.php"><i class="fa fa-table fa-fw"></i> View Products</a>
+                        </li>
+                        <li>
+                            <a href="promo.php"><i class="fa fa-table fa-fw"></i> Promotional Items</a>
                         </li>
                         <li>
                             <a href="update.php"><i class="fa fa-cogs fa-fw"></i> Update Details</a>
