605baaf70b7221bfb7520a37513b2b0bf0a01d65
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-04-28 21:10:10 +0200

    Index.html to index.php
    Heading of the dashboard was changed

--- user/header.php
+++ user/header.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html"><?php echo $sitename; ?> User/a>
+                <a class="navbar-brand" href="index.php"><?php echo $sitename; ?> User</a>
             </div>
             <!-- /.navbar-header -->
 
