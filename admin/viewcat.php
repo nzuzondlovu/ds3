95995f4f00bfbefec68834e9b603cbc4f54cbce2
Aminah Mabaso <mabasoaminah@gmail.com>
2017-05-07 16:32:43 +0200

    Add files via upload

---
+++ admin/viewcat.php
@@ -0,0 +1,210 @@
+<?php
+include 'functions.php';
+?>
+
+<?php
+if(isset($_SESSION['key']) == '' ) {
+	header("location:../login.php");
+}
+?>
+
+<?php
+include 'header.php';
+?>
+
+<!-- Page Content -->
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Categories</h1>
+				<div class="panel panel-default">
+				<form action="viewcat.php" method="POST">
+				<dl>
+				  <dt class="sidebar-search">
+                            <div class="input-group custom-search-form">
+                                <input type="text" name="query" class="form-control" placeholder="Search by ID...">
+                                <span class="input-group-btn">
+                                    <button id="search" name="search" type="submit" class="btn btn-primary">Search</button>
+                                </span>
+                            </div>
+                            <!-- /input-group -->
+                        </dt>
+                        </dl>
+                    
+                        <?php
+                        if(isset($_POST['search']))
+						{
+						echo '<h2>
+						Search Results
+					</h2>';
+						 $name=$_POST['query'];
+						 
+						 $query="SELECT * FROM category WHERE `id`= $name LIMIT 1 ";
+						 
+						 $result =mysqli_query($con,$query);
+						 
+						 if(mysqli_num_rows($result)> 0)
+						 {
+							
+							echo '
+								<table class="table">
+									<thead>
+										<tr>
+											<th>Category ID</th>
+											<th>Name</th>
+											<th>Type</th>
+											<th>Description</th>
+											<th>Date</th>
+										</tr>
+									</thead>
+									<tbody>';
+										while ($row=mysqli_fetch_array($result)) {
+
+											echo '
+											<tr>
+												<td>'.$row['id'].'</td>
+												<td>'.$row['name'].'</td>
+												<td>'.$row['type'].'</td>
+												<td>'.$row['description'].'</td>
+												<td>'.date("M d, y",strtotime($row['dateCreated'])).'</td>
+												<td><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-primary">Delete Category</a>  <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>"
+					
+											</tr>';
+										}
+								
+										echo '
+									</tbody>
+								</table>';
+								}	
+					
+						else
+						{
+							echo " Category Not Found";
+						}	
+						}
+	
+                        ?>
+                    
+                </form>
+                </div>
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						List of all categories
+					</div>
+					<!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="table-responsive">
+							<?php
+							$num_rec_per_page=10;
+
+							if (isset($_GET["page"])) {
+
+								$page  = $_GET["page"];
+							} else {
+
+								$page=1;
+							}
+
+							$start_from = ($page-1) * $num_rec_per_page;
+							$sql = "SELECT * FROM category ORDER BY id DESC LIMIT $start_from, $num_rec_per_page";
+							$res = mysqli_query($con, $sql);
+
+							if (mysqli_num_rows($res) > 0) {
+								echo '
+								<table class="table">
+									<thead>
+										<tr>
+											<th>Category ID</th>
+											<th>Name</th>
+											<th>Type</th>
+											<th>Description</th>
+											<th>Date</th>
+										</tr>
+									</thead>
+									<tbody>';
+										while ($row = mysqli_fetch_assoc($res)) {
+
+											echo '
+											<tr>
+												<td>'.$row['id'].'</td>
+												<td>'.$row['name'].'</td>
+												<td>'.$row['type'].'</td>
+												<td>'.$row['description'].'</td>
+												<td>'.date("M d, y",strtotime($row['dateCreated'])).'</td>
+												<td><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-primary">Delete Category</a>  <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>
+											</tr>';
+										}
+										echo '
+									</tbody>
+								</table>';
+							} else {
+								echo '<div class="alert alert-info">
+								<button type="button" class="close" data-dismiss="alert">&times;</button>
+								<strong>No categories found.</strong>
+							</div>';
+						}
+
+						$sql = "SELECT * FROM category";
+						$rs_result = mysqli_query($con, $sql); 
+						$total_records = mysqli_num_rows($rs_result);  
+						$total_pages = ceil($total_records / $num_rec_per_page);
+
+						if ($total_pages == 0) {
+							$total_pages = 1;
+						}
+
+						echo '
+					</div>
+					<div class="col-lg-12">
+						<p align="center">
+						<a class="btn btn-primary" href="?page=1">'."|<".'</a> '; 
+
+							if ($page < 4) {
+								for ($i=1; $i<$page; $i++) {
+									echo '<a class="btn btn-primary" href="?page='.$i.'">'.$i.'</a> ';
+								};
+							} else {
+								for ($i=($page-3); $i<$page; $i++) {
+									echo '<a class="btn btn-primary" href="?page='.$i.'">'.$i.'</a> ';
+								};
+							}
+							echo '<a class="btn btn-default" href="?page='.$page.'">'.$page.'</a> ';
+
+							if ($page >= ($total_pages - 3)) {
+								for ($i=($page+1); $i<=($total_pages); $i++) {
+									echo '<a class="btn btn-primary" href="?page='.$i.'">'.$i.'</a> ';
+								};
+							} else {
+								for ($i=($page+1); $i<=($page+3); $i++) {
+									echo '<a class="btn btn-primary" href="?page='.$i.'">'.$i.'</a> ';
+								};
+							}
+
+							echo '
+							<a class="btn btn-primary" href="?page='.$total_pages.'">'.">|".'</a>
+						</p>
+					</div>';
+					?>
+				</div>
+				<!-- /.table-responsive -->
+			</div>
+			<!-- /.panel-body -->
+		</div>
+		<!-- /.panel -->
+	</div>
+</div>
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php
+include 'footer.php';
+?>
