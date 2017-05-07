9780c89352d0f36652ad46bf92d0037eff5894ce
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-05-07 19:35:15 +0200

    User Privilleges
    Displays the correct user options/privilleges to the page

--- admin/header.php
+++ admin/header.php
@@ -273,27 +273,10 @@
                             </div>
                             <!-- /input-group -->
                         </li>
-                        <li>
-                            <a href="../admin/"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="bookings.php"><i class="fa fa-table fa-fw"></i> View Bookings</a>
-                        </li>
-                        <li>
-                            <a href="cats.php"><i class="fa fa-edit fa-fw"></i> Categories</a>
-                        </li>
-                        <li>
-                            <a href="additem.php"><i class="fa fa-edit fa-fw"></i> Add Product</a>
-                        </li>
-                        <li>
-                            <a href="items.php"><i class="fa fa-table fa-fw"></i> View Products</a>
-                        </li>
-                        <li>
-                            <a href="promo.php"><i class="fa fa-table fa-fw"></i> Promotional Items</a>
-                        </li>
-                        <li>
-                            <a href="update.php"><i class="fa fa-cogs fa-fw"></i> Update Details</a>
-                        </li>
+                        <?php
+                        $type = $_SESSION['type'];
+                        user($type);
+                        ?>
                     </ul>
                 </div>
                 <!-- /.sidebar-collapse -->
