907e7f74ab60b008059a39833b9b6b3905318de5
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-05-10 11:47:47 +0200

    User Interface update

--- admin/viewcat.php
+++ admin/viewcat.php
@@ -17,77 +17,105 @@ include 'header.php';
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-12">
-				<h1 class="page-header">Categories</h1>
+				<h1 class="page-header">Categories</h1>				
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>
+		<!-- /.row -->
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div>
+					<?php if(isset($_SESSION['failure']) && $_SESSION['failure'] != '') { ?>
+					<div class="alert alert-danger">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<?php echo $_SESSION['failure']; unset($_SESSION['failure']); ?>
+					</div>
+					<?php } ?>
+
+					<?php if(isset($_SESSION['success']) && $_SESSION['success'] != '') { ?>
+					<div class="alert alert-success">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<?php echo $_SESSION['success']; unset($_SESSION['success']); ?>
+					</div>
+					<?php } ?>
+				</div>
 				<div class="panel panel-default">
-				<form action="viewcat.php" method="POST">
-				<dl>
-				  <dt class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" name="query" class="form-control" placeholder="Search by ID...">
-                                <span class="input-group-btn">
-                                    <button id="search" name="search" type="submit" class="btn btn-primary">Search</button>
-                                </span>
-                            </div>
-                            <!-- /input-group -->
-                        </dt>
-                        </dl>
-                    
-                        <?php
-                        if(isset($_POST['search']))
-						{
-						echo '<h2>
-						Search Results
-					</h2>';
-						 $name=$_POST['query'];
-						 
-						 $query="SELECT * FROM category WHERE `id`= $name LIMIT 1 ";
-						 
-						 $result =mysqli_query($con,$query);
-						 
-						 if(mysqli_num_rows($result)> 0)
-						 {
-							
-							echo '
-								<table class="table">
-									<thead>
-										<tr>
-											<th>Category ID</th>
-											<th>Name</th>
-											<th>Type</th>
-											<th>Description</th>
-											<th>Date</th>
-										</tr>
-									</thead>
-									<tbody>';
-										while ($row=mysqli_fetch_array($result)) {
+					<div class="panel-heading">
+						Search for category
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-md-offset-3 col-md-6">
+								<form action="viewcat.php" method="POST">
+									<dl>
+										<dt class="sidebar-search">
+											<div class="input-group custom-search-form">
+												<input type="text" name="query" class="form-control" placeholder="Search by ID...">
+												<span class="input-group-btn">
+													<button id="search" name="search" type="submit" class="btn btn-primary">Search</button>
+												</span>
+											</div>
+											<!-- /input-group -->
+										</dt>
+									</dl>
+
+									<?php
+									if(isset($_POST['search']))
+									{
+										echo '
+										<h2>
+											Search Results
+										</h2>';
+										$name=$_POST['query'];
+
+										$query="SELECT * FROM category WHERE name LIKE '%".$name."%'";
+
+										$result =mysqli_query($con,$query);
+
+										if(mysqli_num_rows($result)> 0) {
 
 											echo '
-											<tr>
-												<td>'.$row['id'].'</td>
-												<td>'.$row['name'].'</td>
-												<td>'.$row['type'].'</td>
-												<td>'.$row['description'].'</td>
-												<td>'.date("M d, y",strtotime($row['dateCreated'])).'</td>
-												<td><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-primary">Delete Category</a>  <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>"
-					
-											</tr>';
-										}
-								
-										echo '
-									</tbody>
-								</table>';
-								}	
-					
-						else
-						{
-							echo " Category Not Found";
-						}	
-						}
-	
-                        ?>
-                    
-                </form>
-                </div>
+											<table class="table">
+												<thead>
+													<tr>
+														<th>Category ID</th>
+														<th>Name</th>
+														<th>Type</th>
+														<th>Description</th>
+														<th>Date</th>
+													</tr>
+												</thead>
+												<tbody>';
+													while ($row=mysqli_fetch_array($result)) {
+
+														echo '
+														<tr>
+															<td>'.$row['id'].'</td>
+															<td>'.$row['name'].'</td>
+															<td>'.$row['type'].'</td>
+															<td>'.$row['description'].'</td>
+															<td>'.date("M d, y",strtotime($row['dateCreated'])).'</td>
+															<td class=" pull-right"><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-danger">Delete Category</a>  <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>
+
+														</tr>';
+													}
+
+													echo '
+												</tbody>
+											</table>';
+										} else {
+											echo " Category Not Found";
+										}	
+									}
+
+									?>
+
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<!-- /.col-lg-12 -->
 		</div>
@@ -138,7 +166,7 @@ include 'header.php';
 												<td>'.$row['type'].'</td>
 												<td>'.$row['description'].'</td>
 												<td>'.date("M d, y",strtotime($row['dateCreated'])).'</td>
-												<td><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-primary">Delete Category</a>  <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>
+												<td class="pull-right"><a href="DeleteCat.php?id='.$row['id'].'" class="btn btn-danger">Delete Category</a>    <a href="EditCat.php?id='.$row['id'].'" class="btn btn-primary">Edit Category</a></td>
 											</tr>';
 										}
 										echo '
@@ -164,7 +192,7 @@ include 'header.php';
 					</div>
 					<div class="col-lg-12">
 						<p align="center">
-						<a class="btn btn-primary" href="?page=1">'."|<".'</a> '; 
+							<a class="btn btn-primary" href="?page=1">'."|<".'</a> '; 
 
 							if ($page < 4) {
 								for ($i=1; $i<$page; $i++) {
