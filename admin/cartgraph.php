cd9dd5484c8ae4618c18a9c5323ca4f05a6d80bc
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-08-12 13:00:07 +0200

    Graph for the cart sales

---
+++ admin/cartgraph.php
@@ -0,0 +1,79 @@
+<?php
+ob_start();
+include '../includes/functions.php';
+?>
+
+<?php
+if(isset($_SESSION['key']) == '' ) {
+	header("location:../login.php");
+}
+?>
+
+<?php
+include 'header.php';
+?>
+<script src="../assets/js/jquery.js" type="text/javascript"></script>
+
+<script src="../assets/js/awesomechart.js" type="application/javascript"></script>
+<!-- Page Content -->
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Cart Sales</h1>
+			</div>
+			<!-- /.col-lg-12 -->
+		</div>
+		<!-- /.row -->
+		<div class="row">
+			<div class="col-lg-12">
+				<div>
+					<?php if(isset($_SESSION['failure']) && $_SESSION['failure'] != '') { ?>
+					<div class="alert alert-danger">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<?php echo $_SESSION['failure']; unset($_SESSION['failure']); ?>
+					</div>
+					<?php } ?>
+
+					<?php if(isset($_SESSION['success']) && $_SESSION['success'] != '') { ?>
+					<div class="alert alert-success">
+						<button type="button" class="close" data-dismiss="alert">&times;</button>
+						<?php echo $_SESSION['success']; unset($_SESSION['success']); ?>
+					</div>
+					<?php } ?>
+				</div>
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						Cart sales to date
+					</div>
+					<!-- /.panel-heading -->
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-12">
+								<div class="pull-right">
+									<a href="cart.php" class="btn btn-success"> Show Cart</a>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<canvas id="stockcanvas">
+									Your web-browser does not support the HTML 5 canvas element.
+								</canvas>
+							</div>
+						</div>
+					</div>
+					<!-- /.panel-body -->
+				</div>
+				<!-- /.panel -->
+			</div>
+		</div>
+	</div>
+	<!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php
+include 'footer.php';
+?>
+<script type="text/javascript" src="../assets/js/cartchart.js"></script>
