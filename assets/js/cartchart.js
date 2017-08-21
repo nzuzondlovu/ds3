5bf9bc9530fe86e79d08d64005b314ed7664afb6
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-08-21 20:52:32 +0200

    Changed chart to pie chart

--- assets/js/cartchart.js
+++ assets/js/cartchart.js
@@ -4,54 +4,47 @@ $(document).ready(function(){
 		methtod: "GET",
 		success: function(data) {
 			console.log(data);
+			Chart.defaults.scale.ticks.beginAtZero = true;
 
-			var supplier = [];
 			var quantity = [];
 			var product = [];
-			var totalprice = [];
+			var total = [];
 
 			for (var i in data) {
 
-				supplier.push(data[i].supplierName);
 				quantity.push(data[i].quantity);
-				product.push(data[i].productName);				
-				totalprice.push(data[i].totalPrice);
+				product.push(data[i].productName);
+				total.push(data[i].totalPrice);
 			}
 
 			var chartdata = {
-				labels: supplier,
+				labels: product,
 				datasets: [
-					{
-						label: 'Quantity',
-						backgroundColor: 'rgba(190, 90, 110, 0.75)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
-						data: quantity
-					},
-					{
-						label: 'Product',
-						backgroundColor: 'rgba(200, 200, 200, 0.75)',
-						borderColor: 'rgba(200, 200, 200, 0.75)',
-						hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
-						hoverBorderColor: 'rgba(200, 200, 200, 1)',
-						data: product
-					}
+				{
+					label: 'Quantity',
+					backgroundColor: ['#f1c40f', '#e67e22', '#16a085'],
+					hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+					hoverBorderColor: 'rgba(200, 200, 200, 1)',
+					data: quantity
+				},
+				{
+					label: 'Total Price',
+					backgroundColor: ['#f1c40f', '#e67e22', '#16a085'],
+					hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+					hoverBorderColor: 'rgba(200, 200, 200, 1)',
+					data: total
+				}
 				]
 			};
 
 			var ctx = $('#stockcanvas');
 
 			var graph = new Chart(ctx, {
-				type: 'bar',
-				data: chartdata, 
+				type: 'pie',
+				data: chartdata,
 				options: {
-					scales: {
-						yAxes: [{
-							ticks: {
-								beginAtZero: true
-							}
-						}]
+					animation: {
+						animateScale: true
 					}
 				}
 			});
