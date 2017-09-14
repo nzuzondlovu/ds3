a1f3531b965a3fc025055bae5c839471157cdae6
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-09-14 14:27:49 +0200

    Update on the ip script

--- includes/ip_locator.php
+++ includes/ip_locator.php
@@ -7,13 +7,13 @@ $date = date("Y-m-d H:i:s");
 
 if($query && $query['status'] == 'success') {
 	
-	echo $sql = 'INSERT INTO views(page, ip, countryCode, country, regionName, city, zip, isp, org, as1, lat, lon, timezone, date) 
+	$sql = 'INSERT INTO views(page, ip, countryCode, country, regionName, city, zip, isp, org, as1, lat, lon, timezone, date) 
 	VALUES ("'.$page.'", "'.$query['query'].'", "'.$query['countryCode'].'", "'.$query['country'].'", "'.$query['regionName'].'", "'.$query['city'].'", "'.$query['zip'].'", "'.$query['isp'].'", "'.$query['org'].'", "'.$query['as'].'", "'.$query['lat'].'", "'.$query['lon'].'", "'.$query['timezone'].'", "'.$date.'")';
 	mysqli_query($con, $sql);
 
 } else {
 
-	echo $sql = 'INSERT INTO views(page, ip, countryCode, country, regionName, city, zip, isp, org, as1, lat, lon, timezone, date) 
+	$sql = 'INSERT INTO views(page, ip, countryCode, country, regionName, city, zip, isp, org, as1, lat, lon, timezone, date) 
 	VALUES ("'.$page.'", "Annonymous", "Annonymous", "Annonymous", "Annonymous", 0, "Annonymous", "Annonymous", "Annonymous", "Annonymous", "Annonymous", "Annonymous", "Annonymous", "'.$date.'")';
 	mysqli_query($con, $sql);
 }
