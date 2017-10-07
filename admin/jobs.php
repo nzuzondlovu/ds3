431224d0afc9b8ceb974a8bbf9176b19d856b113
Nzuzo R. Ndlovu <nzuzondlovu@gmail.com>
2017-10-07 21:59:40 +0200

    Validated and fixed the add device
    I validated and made sure that when you add a new device, you now add a user and device at once

--- admin/jobs.php
+++ admin/jobs.php
@@ -10,43 +10,24 @@ if(isset($_SESSION['key']) == '' ) {
 ?>
 
 <?php
+if(isset($_POST['submit'])) {
 
-if(isset($_POST['customer'])) {
-
-  $name = mysqli_real_escape_string($con, strip_tags(trim($_POST["name"])));
-  $id = mysqli_real_escape_string($con, strip_tags(trim($_POST["id"])));
-  $phone = mysqli_real_escape_string($con, strip_tags(trim($_POST["phone"])));
-  $email = mysqli_real_escape_string($con, strip_tags(trim($_POST["email"])));
-
-  if ($id != '' && $name != '' && $phone != '' && $email != '') {
-
-    $sql = 'INSERT INTO customerrepaire (Cname, idNo, num, email) VALUES("'.$name.'", "'.$id.'", "'.$phone.'", "'.$email.'")';
-    mysqli_query($con, $sql);
-    $_SESSION['success'] = 'User was added successfully.';
-  } else {
-    $_SESSION['failure'] = 'An error occured, please try again.';
-  }
-  
-
-
-}
-?>
-
-<?php
-if(isset($_POST['device'])) {
-
-  $name = mysqli_real_escape_string($con, strip_tags(trim($_POST["name"])));
+  $dname = mysqli_real_escape_string($con, strip_tags(trim($_POST["dname"])));
   $model = mysqli_real_escape_string($con, strip_tags(trim($_POST["model"])));
   $serial = mysqli_real_escape_string($con, strip_tags(trim($_POST["serial"])));
   $type = mysqli_real_escape_string($con, strip_tags(trim($_POST["type"])));
   $date = mysqli_real_escape_string($con, strip_tags(trim($_POST["date"])));
   $price = mysqli_real_escape_string($con, strip_tags(trim($_POST["price"])));
+  $cname = mysqli_real_escape_string($con, strip_tags(trim($_POST["cname"])));
+  $id = mysqli_real_escape_string($con, strip_tags(trim($_POST["id"])));
+  $phone = mysqli_real_escape_string($con, strip_tags(trim($_POST["phone"])));
+  $email = mysqli_real_escape_string($con, strip_tags(trim($_POST["email"])));
   
-  if($name != '' && $model != '' && $serial != '' && $type != '' && $date != '' && $price != '') {
+  if($dname != '' && $model != '' && $serial != '' && $type != '' && $date != '' && $price != '' && $id != '' && $cname != '' && $phone != '' && $email != '') {
 
-    $sql = 'INSERT INTO customersaledevice (diviceName, model, serialNumber, Dtype, recievedDate, establishAmount) VALUES("'.$name.'", "'.$model.'", "'.$serial.'", "'.$type.'", "'.$date.'", "'.$price.'")';
+    $sql = 'INSERT INTO customersaledevice (diviceName, model, serialNumber, Dtype, recievedDate, establishAmount, Cname, idNo, num, email) VALUES("'.$dname.'", "'.$model.'", "'.$serial.'", "'.$type.'", "'.$date.'", "'.$price.'", "'.$cname.'", "'.$id.'", "'.$phone.'", "'.$email.'")';
     mysqli_query($con, $sql);
-    $_SESSION['success'] = 'Your new Promotional Product was added successfully.';
+    $_SESSION['success'] = 'New Customer and Device details was added successfully.';
   }else {
     $_SESSION['failure'] = 'Please fill in all fields.';
   }
@@ -108,41 +89,6 @@ include 'header.php';
     <!-- /.row -->
 
     <!-- Modal -->
-    <div class="modal fade" id="addItem" tabindex="-1" role="dialog" aria-labelledby="addItemLabel">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span class="fa fa-close" aria-hidden="true"></span> Close</button>
-            <h4 class="modal-title" id="addItemLabel">Add Customer</h4>
-          </div>
-          <div class="modal-body">
-            <form role="form" method="post" enctype="multipart/form-data">
-              <div class="form-group">
-                <label>Customer name</label>
-                <input name="name" class="form-control" placeholder="Enter text">
-              </div>
-              <div class="form-group">
-                <label>ID number</label>
-                <input name="id" class="form-control" placeholder="Enter text">
-              </div>
-              <div class="form-group">
-                <label>Phone number</label>
-                <input name="phone" class="form-control" placeholder="Enter text">
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input name="email" class="form-control" placeholder="Enter text">
-              </div>
-              <button name="customer" type="submit" class="btn btn-primary">Submit Customer</button>
-              <button type="reset" class="btn btn-default">Reset Form</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- /.Modal -->
-
-    <!-- Modal -->
     <div class="modal fade" id="addDevice" tabindex="-1" role="dialog" aria-labelledby="addDeviceLabel">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -151,38 +97,66 @@ include 'header.php';
             <h4 class="modal-title" id="addItemLabel">Add Device</h4>
           </div>
           <div class="modal-body">
-            <form role="form" method="post" enctype="multipart/form-data">
-              <div class="form-group">
-                <label>Name</label>
-                <input type="text" name="name" class="form-control" value="">
-              </div>
-              <div class="form-group">
-                <label>Model</label>
-                <input type="text" name="model" class="form-control" value="">
-              </div>
-              <div class="form-group">
-                <label>Serial</label>
-                <input type="text" name="serial" class="form-control" value="">
-              </div>
-              <div class="form-group">
-                <label>Type</label>
-                <select name="type" class="form-control">
-                  <option value="" selected="selected">Select type</option>
-                  <option value="Hardware">Hardware</option>
-                  <option value="Software">Software</option>
-                </select>
-              </div>
-              <div class="form-group">
-                <label>Date</label>
-                <input type="date" name="date" class="form-control" value="">
-              </div>
-              <div class="form-group">
-                <label>Price</label>
-                <input type="number" name="price" class="form-control" value="">
-              </div>
-              <button name="device" type="submit" class="btn btn-primary">Submit Device</button>
-              <button type="reset" class="btn btn-default">Reset Form</button>
-            </form>
+            <div class="row">
+              <form id="waitlistForm" role="form" method="post" enctype="multipart/form-data">
+                <div class="col-md-6">            
+                  <div class="form-group">
+                    <label>Device Name</label>
+                    <input type="text" name="dname" class="form-control" placeholder="Device name">
+                  </div>
+                  <div class="form-group">
+                    <label>Device Model</label>
+                    <input type="text" name="model" class="form-control" placeholder="Device model number">
+                  </div>
+                  <div class="form-group">
+                    <label>Device Serial</label>
+                    <input type="text" name="serial" class="form-control" placeholder="Device serial number">
+                  </div>
+                  <div class="form-group">
+                    <label>Device Type</label>
+                    <select name="type" class="form-control">
+                      <option value="" selected="selected">Select type</option>
+                      <option value="Hardware">Hardware</option>
+                      <option value="Software">Software</option>
+                    </select>
+                  </div>
+                  <div class="form-group">
+                    <label>Price</label>
+                    <input type="number" name="price" class="form-control" placeholder="R0.00">
+                  </div>
+                  <div class="form-group">
+                    <label>Date</label>
+                    <input type="date" name="date" class="form-control" >
+                  </div>                  
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label>Customer name</label>
+                    <input name="cname" class="form-control" placeholder="Customer full name">
+                  </div>
+                  <div class="form-group">
+                    <label>ID number</label>
+                    <input type="number" name="id" class="form-control" placeholder="Customer Identity number">
+                  </div>
+                  <div class="form-group">
+                    <label>Phone number</label>
+                    <input name="phone" class="form-control" placeholder="Customer cell number">
+                  </div>
+                  <div class="form-group">
+                    <label>Email</label>
+                    <input name="email" class="form-control" placeholder="Customer email address">
+                  </div>                          
+                </div>
+                <div class="col-lg-12">
+                  <div class="col-md-6">
+                    <button name="submit" type="submit" class="btn btn-primary btn-block">Submit Device</button>
+                  </div>
+                  <div class="col-md-6">
+                    <button type="reset" class="btn btn-default btn-block">Reset Form</button>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
@@ -215,7 +189,6 @@ include 'header.php';
             <div class="row">
               <div class="col-lg-12">
                 <div class="pull-right">
-                  <button class="btn btn-success" data-toggle="modal" data-target="#addItem"> Add Customer</button>
                   <button class="btn btn-success" data-toggle="modal" data-target="#addDevice"> Add Device</button>
                 </div>
               </div>
@@ -229,43 +202,43 @@ include 'header.php';
               if (mysqli_num_rows($res) > 0) {
                 echo '
                 <table id="bookings" class="table data-table">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Device</th>
-                      <th>Model</th>
-                      <th>serial</th>
-                      <th>type</th>
-                      <th>Amount</th>
-                      <th>Date</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>';
-                    while ($row = mysqli_fetch_assoc($res)) {
+                <thead>
+                <tr>
+                <th>ID</th>
+                <th>Device</th>
+                <th>Model</th>
+                <th>serial</th>
+                <th>type</th>
+                <th>Amount</th>
+                <th>Date</th>
+                <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>';
+                while ($row = mysqli_fetch_assoc($res)) {
 
-                      echo '
-                      <tr>
-                        <td>'.$row['id'].'</td>
-                        <td>'.$row['diviceName'].'</td>
-                        <td>'.$row['model'].'</td>
-                        <td>'.$row['serialNumber'].'</td>
-                        <td>'.$row['Dtype'].'</td>
-                        <td>R '.$row['establishAmount'].'</td>                        
-                        <td>'.date("M d, y",strtotime($row['recievedDate'])).'</td>
-                        <td class="pull-right">
-                          <a class="btn btn-danger" href="?id='.$row['id'].'"><span class="fa fa-trash"> Delete</span></a>
-                          <button class="btn btn-warning" onclick="modal('.$row['id'].')"><span class="fa fa-pencil"> Edit</button>
-                          <!--<a class="btn btn-info" href="checkrepair.php?id='.$row['id'].'"><span class="fa fa-cogs"> Repair device</a>-->
-                        </tr>';
-                      }
-                      echo '
-                    </tbody>
-                  </table>';
-                } else {
-                  echo '<div class="alert alert-info">
-                  <button type="button" class="close" data-dismiss="alert">&times;</button>
-                  <strong>No devices found.</strong>
+                  echo '
+                  <tr>
+                  <td>'.$row['id'].'</td>
+                  <td>'.$row['diviceName'].'</td>
+                  <td>'.$row['model'].'</td>
+                  <td>'.$row['serialNumber'].'</td>
+                  <td>'.$row['Dtype'].'</td>
+                  <td>R '.$row['establishAmount'].'</td>                        
+                  <td>'.date("M d, y",strtotime($row['recievedDate'])).'</td>
+                  <td class="pull-right">
+                  <a class="btn btn-danger" href="?id='.$row['id'].'"><span class="fa fa-trash"> Delete</span></a>
+                  <button class="btn btn-warning" onclick="modal('.$row['id'].')"><span class="fa fa-pencil"> Edit</button>
+                  <!--<a class="btn btn-info" href="checkrepair.php?id='.$row['id'].'"><span class="fa fa-cogs"> Repair device</a>-->
+                  </tr>';
+                }
+                echo '
+                </tbody>
+                </table>';
+              } else {
+                echo '<div class="alert alert-info">
+                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                <strong>No devices found.</strong>
                 </div>';
               }
               ?>
@@ -307,3 +280,97 @@ include 'footer.php';
     $('#bookings').DataTable();
   });
 </script>
+<script type="text/javascript">
+
+    $( document ).ready( function () {
+      $( "#waitlistForm" ).validate( {
+        rules: {
+          dname: {
+            required: true,
+            maxlength: 35
+          },
+          model: {
+            required: true,
+            maxlength: 35
+          },
+          serial: "required",
+          type: "required",
+          price: {
+            required: true,
+            maxlength: 35
+          },
+          date: "required",
+          cname: {
+            required: true,
+            maxlength: 35
+          },
+          id: {
+            required: true,
+            minlength: 13,
+            maxlength: 13
+          },
+          phone: {
+            required: true,
+            minlength: 11,
+            maxlength: 12
+          },
+          email: {
+            required: true,
+            maxlength: 250
+          }
+        },
+        messages: {
+          dname: {
+            required: "Please enter the name of the device",
+            maxlength: "Your device name cant be longer than 35 characters"
+          },
+          model: {
+            required: "Please enter the model of the device",
+            maxlength: "Your model cant be longer than 35 characters"
+          },
+          serial: "Please enter device serial number.",
+          type: "Please choose device type.",
+          price: {
+            required: "Please enter the price of the device",
+            maxlength: "Your price cant be longer than 35 characters"
+          },
+          date: "Please select a date",
+          cname: {
+            required: "Please enter customer name",
+            maxlength: "Your customer name cant be longer than 35 characters"
+          },
+          id: {
+            required: "Please enter user identity number",
+            maxlength: "Your identity number cant be longer than 13 characters",
+            minlength: "Your identity number cant be shorter than 13 characters"
+          },
+          phone: {
+            required: "Please enter customer cell phone number",
+            minlength: "Your customer cell number cant be longer than 11 characters",
+            maxlength: "Your customer cell number cant be longer than 12 characters"
+          },
+          email: {
+            required: "Please enter customer email address",
+            maxlength: "Your description cant be longer than 250 characters"
+          }
+        },
+        errorElement: "em",
+        errorPlacement: function ( error, element ) {
+          // Add the `help-block` class to the error element
+          error.addClass( "help-block" );
+
+          if ( element.prop( "type" ) === "checkbox" ) {
+            error.insertAfter( element.parent( "label" ) );
+          } else {
+            error.insertAfter( element );
+          }
+        },
+        highlight: function ( element, errorClass, validClass ) {
+          $( element ).parents( ".col-sm-5" ).addClass( "has-error" ).removeClass( "has-success" );
+        },
+        unhighlight: function (element, errorClass, validClass) {
+          $( element ).parents( ".col-sm-5" ).addClass( "has-success" ).removeClass( "has-error" );
+        }
+      });
+    });
+  </script>
